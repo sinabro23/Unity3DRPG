20e75acdd334be0b382cb0b1dc0747c6643d081f
jinabro <kmy4869@naver.com>
2022-03-22 03:05:26 +0900

    캐릭터 움직임 및 마우스 이동 구현

--- 3D_RPG/Assets/Script/PlayerController.cs
+++ 3D_RPG/Assets/Script/PlayerController.cs
@@ -5,39 +5,57 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    float _speed = 10.0f;
+    Transform characterBody;
+    [SerializeField]
+    Transform cameraArm;
+
+    Animator animator;
 
     void Start()
     {
+        animator = characterBody.GetComponent<Animator>();
         
     }
 
     void Update()
     {
+        LookAround();
         Move();
+    }
+
+    void LookAround()
+    {
+        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X")* 1.5f, Input.GetAxis("Mouse Y")* 1.5f);
+        Vector3 camAngle = cameraArm.rotation.eulerAngles;
+
+        float x = camAngle.x - mouseDelta.y;
+
+        if(x <180f)
+        {
+            x = Mathf.Clamp(x, -14, 70f);
+        }
+        else
+        {
+            x = Mathf.Clamp(x, 335f, 361f);
+        }
+
+        cameraArm.rotation = Quaternion.Euler(x, camAngle.y + mouseDelta.x, camAngle.z);
     }
 
     void Move()
     {
-        if (Input.GetKey(KeyCode.W))
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        bool isMove = moveInput.magnitude != 0;
+        animator.SetBool("isMove", isMove);
+        if(isMove)
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
-            transform.position += Vector3.forward * _speed * Time.deltaTime;
+            Vector3 lookForward = new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized;
+            Vector3 lookRight = new Vector3(cameraArm.right.x, 0f, cameraArm.right.z).normalized;
+            Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
+
+            characterBody.forward = moveDir;
+            transform.position += moveDir * Time.deltaTime * 5f;
         }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
-            transform.position += Vector3.back * _speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
-            transform.position += Vector3.left * _speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
-            transform.position += Vector3.right * _speed * Time.deltaTime;
-        }
+        Debug.DrawRay(cameraArm.position, new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized, Color.red);
     }
 }
